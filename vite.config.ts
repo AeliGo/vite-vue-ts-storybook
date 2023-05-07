6609a148658c976e1e24dc561a7654c650d0150c
LI WANG <zoeywang1990@gmail.com>
2023-05-07 22:05:28 +1000

    remove logLevel

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import eslintPlugin from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   // clearScreen: false,
-  logLevel: "error",
   plugins: [
     vue(),
     vueI18n({
